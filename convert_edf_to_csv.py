957ca0c53bded08f2bce99fb4f18c86e74f0a5f1
chaynika <chaynika@amagi.com>
2017-11-06 23:41:05 -0700

    Phase 1

--- convert_edf_to_csv.py
+++ convert_edf_to_csv.py
@@ -1,12 +1,18 @@
 import pyedflib
 import numpy as np
 import sys
-filename = sys.argv[1].split('.')[0]+'.csv'
+import pdb
+filename_test = sys.argv[1].split('.')[0]+'_test.csv'
+filename_train = sys.argv[1].split('.')[0]+'_train.csv'
 f=pyedflib.EdfReader(sys.argv[1])
 signal_labels = f.getSignalLabels()
 n = f.signals_in_file
 sigbufs = np.zeros((n, f.getNSamples()[0]))
 for i in np.arange(n):
 	sigbufs[i, :] = f.readSignal(i)
-
-np.savetxt(filename,sigbufs,delimiter=",")
+#pdb.set_trace()
+#sigbufs = np.transpose(sigbufs)
+sigbufs_test = np.hsplit(sigbufs, 2)[0]
+sigbufs_train = np.hsplit(sigbufs, 2)[1]
+np.savetxt(filename_test,sigbufs_test,delimiter=",")
+np.savetxt(filename_train,sigbufs_train,delimiter=",")
